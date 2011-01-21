1fa02a56b1a96c14d2394c966d2ac15347cabced
Guiran Patrick <pguiran@linagora.com>
2011-01-21 17:36:53 +0100

    CLI module support help generator now !
    - new CLI_REGISTER_HELP() function (read usage and description of this function)
    - CLI_REGISTER_MENU() can take a second argument: the help of the group of command of this menu
    - CLI_REGISTER_COMMAND() can take a third argument: the help of the command
    - private_PURIFY_CLI_COMMAND() allow to call CLI_REGISTER_COMMAND() by setting argument of CLI command can be represent by ? or <var> or [<var>]
    - private_CLI_DEFAULT_GET_HELP() is the default function called to get help information of a CLI command is we don't give one when calling CLI_REGISTER_COMMAND() or CLI_REGISTER_MENU()
    - private_CLI_DEFAULT_DISPLAY_HELP() is called for displaying help information of menu, when calling "help" CLI command
    - private_CLI_DEFAULT_DISPLAY_HELP_FOR() is called for display help information of command, when calling "help *" CLI command (* = words)
    - CLI has now 4 command (which can be called from every context menu) : quit, exit, help and help *

--- cli.lib.sh
+++ cli.lib.sh
@@ -31,6 +31,21 @@
 #   usage: CLI_REGISTER_MENU "<cli_menu>"
 #   desc: register a cli menu, which will be added at the beginnig of new CLI command.
 #
+# CLI_REGISTER_HELP()
+#   usage: CLI_REGISTER_HELP <file> [<get_help_func> [ <display_help_func> [ <display_help_for_func> ] ] ]
+#   desc: register a function to call to get help information for a CLI command, and the
+#         cache file where to save help information.
+#   note: if <get_help_func> is not given, use private_CLI_DEFAULT_GET_HELP
+#   note: if <display_help_func> is not given, use private_CLI_DEFAULT_DISPLAY_HELP
+#   note: if <display_help_for_func> is not given, use private_CLI_DEFAULT_DISPLAY_HELP_FOR
+#   note: <get_help_func> is called in two different ways. Here are the usage :
+#         - <get_help_func> 'command' <cli_command> <sh_function> <help>
+#         - <get_help_func> 'menu' <cli_command> <help>
+#   note: last argument given to <get_help_func> can be an empty string
+#   note: <display_help_func> is called when 'help' CLI command is run
+#   note: <display_help_for_func> is called when 'help ?' CLI command is run, ie
+#         'help' CLI command with arguments
+#
 # CLI_SET_PROMPT()
 #   usage: CLI_SET_PROMPT "<string>"
 #   desc: set the CLI prompt
@@ -77,6 +92,7 @@ if [ "${__LIB_CLI__:-}" != 'Loaded' ]; then
 
   load __LIB_MESSAGE__ "${SCRIPT_HELPER_DIRECTORY}/message.lib.sh"
   load __LIB_ASK__     "${SCRIPT_HELPER_DIRECTORY}/ask.lib.sh"
+  load __LIB_RANDOM__  "${SCRIPT_HELPER_DIRECTORY}/random.lib.sh"
 
   # Internal variables
   # ---------------------------------------------------
@@ -85,6 +101,12 @@ if [ "${__LIB_CLI__:-}" != 'Loaded' ]; then
   __CLI_KCODE__=
   __CLI_PROMPT__=
   __CLI_CONTEXT_MENU__=
+
+  __CLI_BUILD_HELP__='false'
+  __CLI_HELP_FILE__=
+  __CLI_GET_HELP__='private_CLI_DEFAULT_GET_HELP'
+  __CLI_DISPLAY_HELP__='private_CLI_DEFAULT_DISPLAY_HELP'
+  __CLI_DISPLAY_HELP_FOR__='private_CLI_DEFAULT_DISPLAY_HELP_FOR'
 
   # determine a good sed separator
   private_SED_SEPARATOR () {
@@ -97,13 +119,39 @@ if [ "${__LIB_CLI__:-}" != 'Loaded' ]; then
     return 1;
   }
 
+  # replace all <var> and [var] pattern into '?'
+  private_PURIFY_CLI_COMMAND () {
+    local result= word= first_char= last_char=
+    echo "$1" | tr ' ' $'\n' \
+      | ( while read word; do
+            [ -z "${word}" ] && continue
+            first_char="${word%"${word#?}"}";
+            if [ "${first_char}" = '<' -o "${first_char}" = '[' ]; then
+              last_char="${word#"${word%?}"}"
+              if [ "${last_char}"  = '>' -o "${last_char}"  = ']' ]; then
+                result="${result} ?"
+                continue;
+              fi
+            fi
+            result="${result} ${word}"
+          done
+          echo -n "${result# }"
+        )
+    return 0
+  }
+
   # build a sed pattern for parsing CLI command
   private_BUILD_SED_COMMAND () {
+    local command= word=
+    command=$( private_PURIFY_CLI_COMMAND "$1" )
     echo -n "^"
-    echo "$1" | tr ' ' $'\n' \
+    echo "${command}" | tr ' ' $'\n' \
       | while read word; do
           [ -z "${word}" ] && continue
-          [ "${word}" = '?' ] && word="\([^ ]*\)"
+          if [ "${word}" = '?' ]; then
+            echo -n ' *\([^ ]*\)'
+            continue
+          fi
           echo -n " *${word}"
         done
     echo " *$"
@@ -112,14 +160,81 @@ if [ "${__LIB_CLI__:-}" != 'Loaded' ]; then
   CLI_SET_PROMPT () { __CLI_PROMPT__="$1"; }
   CLI_USE_READLINE () { ASK_ENABLE_READLINE $@; }
 
+  private_CLI_GET_COMMAND_BASE() {
+    local result=
+    echo "$1" | tr ' ' $'\n' \
+      | ( while read word; do
+            [ -z "${word}" ] && continue
+            [ "${word}" = '?' ] && break
+            result="${result} ${word}"
+         done
+         echo "${result# }"
+        )
+    return 0
+  }
+
+  private_CLI_DEFAULT_GET_HELP() {
+    local type="$1" cli_command="$2" help=
+    [ "${type}" = 'command' ] && help="$4" || help="$3"
+    if [ "${type}" = 'menu' ]; then
+      cli_command=$( private_PURIFY_CLI_COMMAND "${cli_command}" )
+      cli_command=$( private_CLI_GET_COMMAND_BASE "${cli_command}" )
+    fi
+    printf "%s\t%s\t%s" "${type}" "${cli_command}" "${help}"
+    return 0;
+  }
+
+  private_CLI_DEFAULT_DISPLAY_HELP () {
+    grep $'^menu\t' < "${__CLI_HELP_FILE__}" \
+      | cut -d $'\t' -f 2-
+  }
+
+  private_CLI_DEFAULT_DISPLAY_HELP_FOR () {
+    grep $'^command' < "${__CLI_HELP_FILE__}" \
+      | cut -d $'\t' -f 2-                    \
+      | grep "^$* "
+  }
+
+  CLI_REGISTER_HELP () {
+    [ $# -lt 1 -o $# -gt 4 ] && ERROR "CLI_REGISTER_HELP: invalid arguments"
+    [ -z "$1" ] && ERROR "CLI_REGISTER_HELP: arguments are empty"
+
+    __CLI_HELP_FILE__="$1"
+    [ $# -gt 1 ] && __CLI_GET_HELP__="$2"
+    [ $# -gt 2 ] && __CLI_DISPLAY_HELP__="$3"
+    [ $# -gt 3 ] && __CLI_DISPLAY_HELP_FOR__="$4"
+
+    if [ ! -f "${__CLI_HELP_FILE__}" ]; then
+      touch "${__CLI_HELP_FILE__}" || ERROR "CLI_REGISTER_HELP: can't create help file"
+      __CLI_BUILD_HELP__='true'
+    else
+      __CLI_BUILD_HELP__='false'
+    fi
+    return 0;
+  }
+
+  private_CLI_SAVE_HELP () {
+    [ $# -ne 1 ] && ERROR "private_CLI_SAVE_HELP: invalid arguments"
+    [ "${__CLI_BUILD_HELP__}" = 'false' ] && return;
+    grep "$1" < "${__CLI_HELP_FILE__}" >/dev/null 2>/dev/null
+    [ $? -ne 0 ] && echo "$1" >> "${__CLI_HELP_FILE__}"
+    return 0
+  }
+
   CLI_REGISTER_COMMAND () {
-    [ $# -ne 2 ] && ERROR "CLI_REGISTER_COMMAND: invalid arguments"
+    [ $# -ne 2 -a $# -ne 3 ] && ERROR "CLI_REGISTER_COMMAND: invalid arguments"
     private_CLI_COMPIL "$1" "$2" "__CLI_CODE__"
+    if [ "${__CLI_BUILD_HELP__}" = 'true' ]; then
+      private_CLI_SAVE_HELP "$( eval "${__CLI_GET_HELP__} 'command' '$1' '$2' '${3:-}'" )"
+    fi
   }
 
   CLI_REGISTER_KCOMMAND() {
-    [ $# -ne 2 ] && ERROR "CLI_REGISTER_KCOMMAND: invalid arguments"
+    [ $# -ne 2 -a $# -ne 3 ] && ERROR "CLI_REGISTER_KCOMMAND: invalid arguments"
     private_CLI_COMPIL "$1" "$2" "__CLI_KCODE__"
+    if [ "${__CLI_BUILD_HELP__}" = 'true' ]; then
+      private_CLI_SAVE_HELP "$( eval "${__CLI_GET_HELP__} 'command' '$1' '$2' '${3:-}'" )"
+    fi
   }
 
   private_CLI_COMPIL() {
@@ -148,8 +263,10 @@ if [ "${__LIB_CLI__:-}" != 'Loaded' ]; then
     cli_menu=$( private_BUILD_SED_COMMAND "${cli_menu}" )
 
     __CLI_CODE__="${__CLI_CODE__} s${sep}\(${cli_menu}\)${sep}CLI_ENTER_MENU \1${sep}p; t;"
+    if [ "${__CLI_BUILD_HELP__}" = 'true' ]; then
+      private_CLI_SAVE_HELP "$( eval "${__CLI_GET_HELP__} 'menu' '$1' '${2:-}' ")"
+    fi
   }
-
 
   CLI_RUN_COMMAND () {
     local cmd=
@@ -177,6 +294,8 @@ if [ "${__LIB_CLI__:-}" != 'Loaded' ]; then
 
     # internal CLI special commands
     kcode="${__CLI_KCODE__}"
+    kcode="${kcode} s/^ *help *$/private_CLI_DEFAULT_DISPLAY_HELP/p; t;"
+    kcode="${kcode} s/^ *help *\(.*\)$/private_CLI_DEFAULT_DISPLAY_HELP_FOR \1/p; t;"
     kcode="${kcode} s/^ *quit *$/${CLI_QUIT}/p; t;"
     kcode="${kcode} s/^ *exit *$/break/p; t;"
 
